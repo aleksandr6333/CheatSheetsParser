69302fd4b0a2ae7cbcf6a14c6ab069ddbd40027a
Aleksandr <aleksandr6333@gmail.com>
2022-10-19 15:10:40 +0400

    melody 1.2

--- utils.py
+++ utils.py
@@ -1,22 +1,24 @@
 import shelve
 from SQLighter import SQLighter
 from config import shelve_name, database_name
+from random import shuffle
+from telebot import types
 
 
 def count_rows():
     # метод считает общее число строк в БД и сохраняет в хранилище.
     # Далее из него выбирается музыка
     db = SQLighter(database_name)
-    rowsum = db.count_rows()
+    rowsnum = db.count_rows()
     with shelve.open(shelve_name) as storage:
-        storage['row_counts'] = rowsum
+        storage['rows_counts'] = rowsnum
 
 
 def get_rows_count():
     # Получаем из хранилища число строк в БД (:return: (int))
     with shelve.open(shelve_name) as storage:
-        rowsum = storage['rows_count']
-    return rowsum
+        rowsnum = storage['rows_count']
+    return rowsnum
 
 def set_user_game(chat_id, estimated_answers):
     # записываем пользователя в игроки и запоминаем ответ
@@ -37,3 +39,19 @@ def get_answer_for_user(chat_id):
 
         except KeyError:
             return None
+
+
+
+
+
+def generate_markup(right_answer, wrong_answers):
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    all_answers = '{},{}'.format(right_answer, wrong_answers)
+    list_items = []
+    for item in all_answers.split(','):
+        list_items.append(item)
+    shuffle(list_items)
+    for item in list_items:
+        markup.add(item)
+    return markup
+
